a13932d6dd7728b9a1119e61d2e4c6317953d08e
Lel Illumina <dragonbilal938@outlook.com>
2025-06-22 22:16:53 +0500

    feat: add mdx sitemap and unplugin icons

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,44 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+
+import Icons from "unplugin-icons/vite";
+
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  prefetch: {
+    prefetchAll: true,
+    defaultStrategy: "viewport",
+  },
+  site: "https://lel.nekoweb.org",
+  output: "static",
+  server: { host: true },
+
+  integrations: [mdx(), sitemap({ xslURL: "/sitemap.xsl" })],
+
+  vite: {
+    build: {
+      target: "esnext",
+      minify: "esbuild",
+      cssCodeSplit: true,
+      assetsInlineLimit: 0,
+    },
+    optimizeDeps: {
+      include: [],
+    },
+    esbuild: {
+      treeShaking: true,
+      drop: ["console", "debugger"],
+    },
+    plugins: [Icons({ compiler: "astro" })],
+  },
+
+  experimental: {
+    contentIntellisense: true,
+    clientPrerender: true,
+    // svg: true,
+  },
+});
