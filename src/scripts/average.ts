0c5146e573def5f0afd84cc9b90320da44391d05
Lel Illumina <dragonbilal938@outlook.com>
2025-06-27 09:03:36 +0500

    feat: run fast-average-color on lastfm album cover

--- src/scripts/average.ts
+++ src/scripts/average.ts
@@ -5,16 +5,16 @@ const fac = new FastAverageColor();
 const figures = document.querySelectorAll("figure");
 
 for (const figure of figures) {
-	const image = figure.querySelector("img");
+  const image = figure.querySelector("img");
 
-	if (!image) continue;
+  if (!image) continue;
 
-	fac
-		.getColorAsync(image, { algorithm: "simple" })
-		.then((color) => {
-			image.style.filter = `drop-shadow(0 0 10px var(--night)) drop-shadow(0 0 40px ${color.rgba})`;
-		})
-		.catch((e) => {
-			console.error("Color extraction failed:", e);
-		});
+  fac
+    .getColorAsync(image, { algorithm: "simple" })
+    .then((color) => {
+      image.style.filter = `drop-shadow(0 0 10px var(--night)) drop-shadow(0 0 40px ${color.rgba})`;
+    })
+    .catch((e) => {
+      console.error("Color extraction failed:", e);
+    });
 }
