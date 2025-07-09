1b19ca1de7e73ea8deeaeccdade9400d4845e9ce
Lel Illumina <dragonbilal938@outlook.com>
2025-07-09 05:57:24 +0500

    feat: Separate Guide page

--- src/content.config.ts
+++ src/content.config.ts
@@ -15,6 +15,19 @@ const blogs = defineCollection({
       draft: z.boolean(),
     }),
 });
+const guides = defineCollection({
+  loader: glob({ pattern: "**/*.{md,mdx}", base: "./src/content/guides" }),
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      description: z.string(),
+      date: z.coerce.date(),
+      thumb: image(),
+      tags: z.array(z.string()),
+      keywords: z.array(z.string()).optional(),
+      draft: z.boolean(),
+    }),
+});
 const archive = defineCollection({
   loader: glob({
     pattern: "**/*.{md,mdx}",
@@ -28,4 +41,4 @@ const archive = defineCollection({
   }),
 });
 
-export const collections = { blogs, archive };
+export const collections = { blogs, guides, archive };
